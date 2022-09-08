c71fbb17b32d67e99c2dbb00bd4974a6a0b1620e
Atsushi-Kakuta <invest9me@yahoo.co.jp>
2022-09-08 23:02:24 +0900

    final final update

--- jobcan_auto_login.py
+++ jobcan_auto_login.py
@@ -11,13 +11,15 @@ jobcan_login_password = os.getenv("JOBCAN_LOGIN_PASSWORD")
 
 def jobcan_automation(arg):
     try:
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+
         driver.get("https://id.jobcan.jp/users/sign_in")
         sleep(1)
 
         login_id = driver.execute_script("return document.querySelector('#user_email')").send_keys(jobcan_login_id)
         sleep(1)
-
         password = driver.execute_script("return document.querySelector('#user_password')").send_keys(
             jobcan_login_password)
         sleep(1)
@@ -26,14 +28,20 @@ def jobcan_automation(arg):
         sleep(1)
 
         driver.execute_script("return document.querySelector('#jbc-app-links > ul > li:nth-child(3) > a')").click()
-        sleep(1)
+        sleep(5)
+        handle_array = driver.window_handles
+        driver.switch_to.window(handle_array[1])
         if arg == 0:
             driver.execute_script("return document.querySelector('#adit-button-work-start')").click()
-            return
+            sleep(1)
         if arg == 1:
             driver.execute_script("return document.querySelector('#adit-button-work-end')").click()
+            sleep(1)
         driver.close()
         return 1
 
     except:
         return 0
+
+
+jobcan_automation(0)
